5ab7cc60280d923b9cf2d8c85954fcfa8fff08f9
Anthony <crayonno.o@gmail.com>
2022-08-11 01:30:57 +0100

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -54,6 +54,8 @@ const login = async (req, res) => {
     res
       .cookie("access_token", token, {
         httpOnly: true,
+        sameSite: "none",
+        secure: true,
       })
       .status(200)
       .json({ ...others });
