4b77558b1a5d271beaf737f87812ad120cba109a
Anthony <crayonno.o@gmail.com>
2022-08-16 01:44:13 +0100

    deep

--- controllers/users.js
+++ controllers/users.js
@@ -92,18 +92,20 @@ const followUser = async (req, res) => {
 
 const unfollowUser = async (req, res) => {
   if (req.body.userId === req.params.id) {
-    res.status(401).json("You can not unfollow yourself");
+    res.status(500).json("You can't unfollow yourself");
   } else {
-    const currentUser = await Users.findById(req.params.id);
-    const userToUnfollow = await Users.findById(req.body.userId);
-    if (currentUser.followings.includes(userToUnfollow._Id)) {
-      await currentUser.updateOne({
-        $pull: { followings: userToUnfollow._id },
-      });
-      await currentUser.updateOne({ $pull: { followers: currentUser._id } });
-      res.status(200).json("User has been unfollowed");
-    } else {
-      return res.status(500).json("You are not following this user");
+    try {
+      const currentUser = await Users.findById(req.params.id);
+      const userToUnfollow = await Users.findById(req.body.userId);
+      if (!userToUnfollow.followers.includes(currentUser._id)) {
+        res.status(500).json("You are not following this user");
+      } else {
+        await userToUnfollow.updateOne({ $pull: { followers: currentUser._id } });
+        await currentUser.updateOne({ $pull: { followings: req.body.userId } });
+        res.status(200).json("User has been followed");
+      }
+    } catch (error) {
+      res.status(500).json(error);
     }
   }
 };
