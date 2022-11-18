e759b29ebd7344efa117be24bd067f3d3dcd894b
Anthony <58951485+EzehAntony@users.noreply.github.com>
2022-11-18 15:05:22 +0100

    update

--- index.js
+++ index.js
@@ -12,26 +12,19 @@ app.use(cookie_parser());
 app.use(express.json());
 app.set("trust proxy", 1);
 mongoose
-  .connect(process.env.url)
-  .then(() => {
-    console.log("Successfully connected to the database.");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.url)
+    .then(() => {
+        console.log("Successfully connected to the database.");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.use(
-  cors({
-    credentials: true,
-    origin: [
-      "http://localhost:3000",
-      "https://droplike.netlify.app",
-      "http://droplike.herokuapp.com",
-      "https://droplikebackend.herokuapp.com",
-      "https://droplike.vercel.app",
-      "https://droplike-crayonne.vercel.app"
-    ],
-  })
+    cors({
+        credentials: true,
+        origin: "*",
+    })
 );
 
 app.use("/api/auth", authRoute);
@@ -39,5 +32,5 @@ app.use("/api/user", userRoute);
 app.use("/api/post", postRoute);
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server is now running");
+    console.log("Backend server is now running");
 });
